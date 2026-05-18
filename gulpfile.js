604f298f3b56df3d75492aaf60c31a7408c511f6
Neslom <syozya.mod@gmail.com>
2026-05-18 19:40:27 +0300

    refactor: convert CSS to SCSS with modular architecture and Gulp build

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+import gulp from 'gulp';
+import * as dartSass from 'sass';
+import gulpSass from 'gulp-sass';
+import autoprefixer from 'gulp-autoprefixer';
+import browserSync from 'browser-sync';
+
+const sass = gulpSass(dartSass);
+const bs = browserSync.create();
+
+function styles() {
+	return gulp
+		.src('scss/main.scss', { sourcemaps: true })
+		.pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
+		.pipe(autoprefixer())
+		.pipe(gulp.dest('.', { sourcemaps: '.' }))
+		.pipe(bs.stream());
+}
+
+function serve() {
+	bs.init({
+		server: { baseDir: '.' },
+		notify: false,
+	});
+
+	gulp.watch('scss/**/*.scss', styles);
+	gulp.watch('*.html').on('change', bs.reload);
+}
+
+export const build = styles;
+export default serve;
